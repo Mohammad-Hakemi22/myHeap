f714aebdde3ee5451e263ef6c0e59e271d9e504c
Mohammad-Hakemi22 <hakemi.mohammad.22@gmail.com>
2024-04-25 13:49:50 +0330

    add simple mmalloc function

--- main.c
+++ main.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdint.h>
+
+void *mmalloc(size_t size);
 
 int main(void) {
-    printf("hello world!\n");
-    printf("%x", sbrk(0));
+    int* arr = (int*)mmalloc(sizeof(int)*10);
+    for (int i = 0; i < 10; ++i) {
+        arr[i] = i;
+    }
+    printf("%d\n", arr);
+    printf("%d\n", arr[5]);
+    printf("%d\n", &arr[5]);
     return 0;
+}
+
+
+void *mmalloc(size_t size) {
+    void *newBlock;
+    newBlock = sbrk(size);
+    if (newBlock == (void*)-1) {
+        return NULL;
+    }
+    return newBlock;
 }
